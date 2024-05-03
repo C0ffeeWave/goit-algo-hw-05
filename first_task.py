30c76cfb18749f4b2519b9014a0dfee96873b58a
Wave <ert4.51c@gmail.com>
2024-05-03 19:55:28 +0300

    first_tasl_resolution

--- first_task.py
+++ first_task.py
@@ -1,1 +1,20 @@
+def caching_fibonacci():
+    # Create an empty dictionary
+    cache = {}
 
+    # Define the inner function fibonacci
+    def fibonacci(n):
+        # Base cases
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        # Check cache
+        elif n in cache:
+            return cache[n]
+
+        # Compute Fibonacci recursively
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
