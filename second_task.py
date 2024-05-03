fb7a10cd95db53740389d0173ca9110ce6518f89
Wave <ert4.51c@gmail.com>
2024-05-03 22:45:05 +0300

    second_task

--- second_task.py
+++ second_task.py
@@ -1,1 +1,27 @@
-text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+import re
+from typing import Callable
+
+def generator_numbers(text: str):
+    # Finding of floating-point numbers
+    pattern = r"[-+]?\d*\.?\d+"
+    
+    # Finding all numbers matching the pattern 
+    matches = re.findall(pattern, text)
+    
+    # Creating of generator 
+    for match in matches:
+        yield float(match)  
+
+def sum_profit(text: str, func: Callable):
+    # Calling of generator function 
+    numbers_generator = func(text)
+    
+    # Summing numbers in the generator
+    total_sum = sum(numbers_generator)
+    
+    return total_sum
+
+# Test
+text = "The total income of an employee consists of several parts: 1000.01 as the main income, supplemented with additional revenues of 27.45 and 324.00 dollars."
+total_income = sum_profit(text, generator_numbers)
+print(f"Total income: {total_income}")
